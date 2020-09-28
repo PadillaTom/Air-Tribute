0fda121955e8395be61eb15fd18623ac153cd3f3
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-28 18:36:23 +0200

    Mobile styles, Nav Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,23 +9,40 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className='header-container'>
-      <Link to='/'>
-        <div className='header-logo'>
-          <h1>
-            <span>Air</span>
-            Tribute
-          </h1>
+      {/* Header Mobile Only */}
+      <div className='header-mobile-only-container'>
+        <div className='mobile-header-top'>
+          <Link to='/'>
+            <div className='header-logo'>
+              <h1>
+                <span>Air</span>
+                Tribute
+              </h1>
+            </div>
+          </Link>
+          <Avatar className='header-info-icon' id='avatar-header'></Avatar>
         </div>
-      </Link>
-      <div className='header-search-container'>
-        <input type='text' />
-        <SearchIcon></SearchIcon>
       </div>
-      <div className='header-info-container'>
-        <p>Become Host</p>
-        <LanguageIcon className='header-info-icon'></LanguageIcon>
-        <ExpandMoreIcon className='header-info-icon'></ExpandMoreIcon>
-        <Avatar className='header-info-icon' id='avatar-header'></Avatar>
+      {/* End Header Mobile Only */}
+      <div className='header-query-container'>
+        <Link to='/'>
+          <div className='header-logo'>
+            <h1>
+              <span>Air</span>
+              Tribute
+            </h1>
+          </div>
+        </Link>
+        <div className='header-search-container'>
+          <input type='text' />
+          <SearchIcon></SearchIcon>
+        </div>
+        <div className='header-info-container'>
+          <p>Become Host</p>
+          <LanguageIcon className='header-info-icon'></LanguageIcon>
+          <ExpandMoreIcon className='header-info-icon'></ExpandMoreIcon>
+          <Avatar className='header-info-icon' id='avatar-header'></Avatar>
+        </div>
       </div>
     </header>
   );
